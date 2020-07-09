c19795e64bdaf92dd0023672d55fb9a1e13e3276
PavelLazarchuk <pashalazarchuk@gmail.com>
2020-07-09 23:38:11 +0300

    Updates

---
+++ precache-manifest.194a6bcb55c1e1d14a9292a4d9e73907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78c3183d9e54f7d5073e9d8bf3b8c58",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "27882775f7a13a7d9d2b",
+    "url": "/star-wars/static/css/main.06111f66.chunk.css"
+  },
+  {
+    "revision": "0380a192abe75cf01a68",
+    "url": "/star-wars/static/js/2.da6a8d88.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/star-wars/static/js/2.da6a8d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27882775f7a13a7d9d2b",
+    "url": "/star-wars/static/js/main.8451a6db.chunk.js"
+  },
+  {
+    "revision": "170ef3f5f44284a63889",
+    "url": "/star-wars/static/js/runtime-main.fd5ab69c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/starwars/precache-manifest.36bbbafc599ca9a9b68b2255f71aa513.js"
+  "/star-wars/precache-manifest.194a6bcb55c1e1d14a9292a4d9e73907.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starwars/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/star-wars/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
