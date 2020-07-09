36c501344b30f25f17ed3c244714c0ecfb1562d3
PavelLazarchuk <pashalazarchuk@gmail.com>
2020-07-09 23:31:41 +0300

    Updates

---
+++ precache-manifest.34156b3eb7b78541c6f03235b5d5c9c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7a2ad7f5fde61c1b8181cc18193b1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27882775f7a13a7d9d2b",
+    "url": "/static/css/main.06111f66.chunk.css"
+  },
+  {
+    "revision": "0380a192abe75cf01a68",
+    "url": "/static/js/2.da6a8d88.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.da6a8d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27882775f7a13a7d9d2b",
+    "url": "/static/js/main.8451a6db.chunk.js"
+  },
+  {
+    "revision": "951347fea0fa77c1dae0",
+    "url": "/static/js/runtime-main.08e0f2e8.js"
+  }
+]);
